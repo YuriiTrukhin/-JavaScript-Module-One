045ecf52e28710269973fd91fa4e4753e1d81a6a
YuriiTrukhin <TPYXIH@gmail.com>
2020-09-24 21:03:02 +0300

    добавляет первую и вторую задачу

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// // ----------------------------example------------------;
+// let number = 0;
+// let sum = 0;
+
+// for (let i = 0; i < 10; i += 1) {
+//   number = prompt("Enter your number:");
+//   sum = sum + Number(number);
+
+//   console.log(`Your number: ${number}, current sum: ${sum}`);
+// }
+
+// console.log(`Total sum: ${sum}`);
